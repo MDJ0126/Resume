960e1c1bfc78cb7b413263a265bdbe784e7b18cb
문동준 <ehdwns0126@gmail.com>
2025-03-02 22:43:42 +0900

    기본 템플릿

---
+++ ~local_server_open.bat
@@ -0,0 +1,18 @@
+@echo off
+
+set BASEURL=Portfolio
+
+echo Installing bundles...
+call bundle install
+
+echo Adding webrick...
+call bundle add webrick
+
+:: Jekyll 서버가 실행된 후, 브라우저를 열고 특정 주소로 이동
+start "" "http://127.0.0.1:4000/%BASEURL%/"
+
+echo Executing Jekyll serve...
+call bundle exec jekyll serve --trace
+
+echo Done.
+pause
